603b6e8627e2b1d104375488ffa02427800e918b
Roey Darwish Dror <roey.ghost@gmail.com>
2018-08-26 16:29:47 +0300

    Use check instead of build in the CI

--- ci/script.sh
+++ ci/script.sh
@@ -5,8 +5,8 @@ set -ex
 # TODO This is the "test phase", tweak it as you see fit
 main() {
     cargo fmt --all -- --check
-    cross build --target $TARGET
-    cross build --target $TARGET --release
+    cross check --target $TARGET
+    cross check --target $TARGET --release
 
     if [ ! -z $DISABLE_TESTS ]; then
         return
