f33d0e69360d84a05beb1e6d1450c3954e3fcadb
Roey Darwish Dror <roey.ghost@gmail.com>
2019-01-21 22:38:47 +0200

    Shorten the test phase in the CI

--- ci/script.sh
+++ ci/script.sh
@@ -5,11 +5,8 @@ set -ex
 # TODO This is the "test phase", tweak it as you see fit
 main() {
     cargo fmt --all -- --check
+    cross clippy --all-targets -- -D warnings
     cross clippy --all-targets --all-features -- -D warnings
-    cross check --target $TARGET
-    cross check --target $TARGET --release
-    cross check --target $TARGET --all-features
-    cross check --target $TARGET --release --all-features
 
     if [ ! -z $DISABLE_TESTS ]; then
         return
