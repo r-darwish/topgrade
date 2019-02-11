ca6e12a87d952896ad2f05a65932b60d71da5388
Roey Darwish Dror <roey.ghost@gmail.com>
2019-02-11 14:15:11 +0200

    require_option is only required in Linux

--- src/utils.rs
+++ src/utils.rs
@@ -181,6 +181,7 @@ pub fn require<T: AsRef<OsStr> + Debug>(binary_name: T) -> Result<PathBuf, Error
     }
 }
 
+#[cfg(linux)]
 pub fn require_option<T>(option: Option<T>) -> Result<T, Error> {
     if let Some(value) = option {
         Ok(value)
