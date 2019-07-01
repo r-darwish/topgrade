6874b1bb9c4e3961699ae376147cb1fe02a20800
Roey Darwish Dror <roey.ghost@gmail.com>
2019-07-01 09:43:53 +0300

    Run void upgrade twice (fix #170)

--- src/steps/os/linux.rs
+++ src/steps/os/linux.rs
@@ -162,10 +162,12 @@ fn upgrade_suse(sudo: &Option<PathBuf>, run_type: RunType) -> Result<(), Error> 
 
 fn upgrade_void(sudo: &Option<PathBuf>, run_type: RunType) -> Result<(), Error> {
     if let Some(sudo) = &sudo {
-        run_type
-            .execute(&sudo)
-            .args(&["/usr/bin/xbps-install", "-Su"])
-            .check_run()?;
+        for _ in 0..2 {
+            run_type
+                .execute(&sudo)
+                .args(&["/usr/bin/xbps-install", "-Su"])
+                .check_run()?;
+        }
     } else {
         print_warning("No sudo detected. Skipping system upgrade");
     }
