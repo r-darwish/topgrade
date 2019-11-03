59dbf68bb78ccb2c6e3228938c1070d8ac4461a5
Roey Darwish Dror <roey.ghost@gmail.com>
2019-11-03 14:44:09 +0200

    Support Emacs straight (fix #225)

--- src/steps/emacs.el
+++ src/steps/emacs.el
@@ -1,14 +1,19 @@
-(if (fboundp 'paradox-upgrade-packages)
-    (progn
-      (paradox-upgrade-packages)
-      (princ
-       (if (get-buffer "*Paradox Report*")
-           (with-current-buffer "*Paradox Report*" (buffer-string))
-         "\nNothing to upgrade\n")))
-  (progn
-    (let ((package-menu-async nil))
-      (package-list-packages))
-    (package-menu-mark-upgrades)
-    (condition-case nil
-        (package-menu-execute 'noquery)
-      (user-error nil))))
+(cond
+ ((fboundp 'paradox-upgrade-packages)
+  (princ "Upgrading packages with Paradox")
+  (paradox-upgrade-packages)
+  (princ
+   (if (get-buffer "*Paradox Report*")
+       (with-current-buffer "*Paradox Report*" (buffer-string))
+     "\nNothing to upgrade\n")))
+ ((fboundp 'straight-thaw-versions)
+  (princ "Thawing versions")
+  (straight-thaw-versions))
+ (t
+  (princ "Upgrading packages")
+  (let ((package-menu-async nil))
+    (package-list-packages))
+  (package-menu-mark-upgrades)
+  (condition-case nil
+      (package-menu-execute 'noquery)
+    (user-error nil))))
