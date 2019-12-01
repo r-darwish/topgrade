d577d76528b2ad61dc1d215f344982e0dd7e304e
Kyle Copperfield <kmcopper@danwin1210.me>
2019-12-01 06:40:50 +0000

    Move rpi-update under config.should_run(Step::Firmware) (#270)
    Fixes #269.

--- src/main.rs
+++ src/main.rs
@@ -543,12 +543,6 @@ fn run() -> Result<(), Error> {
                 || linux::run_pihole_update(sudo.as_ref(), run_type),
                 config.no_retry(),
             )?;
-            execute(
-                &mut report,
-                "rpi-update",
-                || linux::run_rpi_update(sudo.as_ref(), run_type),
-                config.no_retry(),
-            )?;
         }
 
         if config.should_run(Step::Firmware) {
@@ -556,6 +550,12 @@ fn run() -> Result<(), Error> {
                 &mut report,
                 "Firmware upgrades",
                 || linux::run_fwupdmgr(run_type),
+                config.no_retry(),
+            )?;
+            execute(
+                &mut report,
+                "rpi-update",
+                || linux::run_rpi_update(sudo.as_ref(), run_type),
                 config.no_retry(),
             )?;
         }
