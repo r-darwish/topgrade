7848b78a575beb5f5c5accb39098df6a970f40bd
Linux4Life531 <61314680+Linux4Life531@users.noreply.github.com>
2020-02-22 15:55:57 +0000

    Create MustBeRanBeforeUsing.sh

---
+++ MustBeRanBeforeUsing.sh
@@ -0,0 +1,11 @@
+
+echo This will place the man page in its folder
+
+cd /usr/local/man/
+mkdir man8
+
+cd -
+
+sudo cp topgrade.8.gz /usr/local/man/man8/
+
+echo Boom, the man topgrade command now works on your current machine. Thanks.
