ba1cd52181c69f6ffab14e4644023a69dac45fc5
Linux4Life531 <61314680+Linux4Life531@users.noreply.github.com>
2020-02-23 11:54:05 +0000

    Update MustBeRanBeforeUsingIfOnLinux.sh

--- MustBeRanBeforeUsingIfOnLinux.sh
+++ MustBeRanBeforeUsingIfOnLinux.sh
@@ -9,12 +9,4 @@ cd -
 
 sudo cp topgrade.8.gz /usr/local/man/man8/
 
-echo Boom, the man topgrade command now works on your current machine.
-
-echo Adding the TopgradeTime command
-
-chmod 777 TimeTopgrade
-
-export PATH=$PATH:.
-
-echo Thanks for downloading this package
+echo Boom, the man topgrade command now works on your current machine. Thanks For Downloading
