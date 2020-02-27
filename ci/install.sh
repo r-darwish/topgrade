a5e8ad68f8611a911671eb85a22b472fa3e247b1
Roey Darwish Dror <roey.ghost@gmail.com>
2020-02-27 20:58:09 +0200

    Use 0.1 branch of cross (#348)

--- ci/install.sh
+++ ci/install.sh
@@ -30,17 +30,11 @@ main() {
             ;;
     esac
 
-    # This fetches latest stable release
-    local tag=$(git ls-remote --tags --refs --exit-code https://github.com/japaric/cross \
-                       | cut -d/ -f3 \
-                       | grep -E '^v[0.1.0-9.]+$' \
-                       | $sort --version-sort \
-                       | tail -n1)
     curl -LSfs https://japaric.github.io/trust/install.sh | \
         sh -s -- \
            --force \
            --git japaric/cross \
-           --tag $tag \
+           --tag v0.1.16 \
            --target $target
 }
 
