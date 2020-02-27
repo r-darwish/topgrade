ef0b9fa2cd8df9b375907d7c03cefaaf0a3cf589
Roey Darwish Dror <roey.ghost@gmail.com>
2020-02-27 20:17:03 +0200

    Print the body of the release

--- src/self_update.rs
+++ src/self_update.rs
@@ -29,7 +29,9 @@ pub fn self_update() -> Result<()> {
 
     if let UpdateStatus::Updated(release) = &result {
         println!("\nTopgrade upgraded to {}:\n", release.version);
-        println!("{}", release.name);
+        if let Some(body) = &release.body {
+            println!("{}", body);
+        }
     } else {
         println!("Topgrade is up-to-date");
     }
