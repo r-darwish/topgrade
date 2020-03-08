3b4de14086b8d55dd1839f16153f981597a19fe7
Roey Darwish Dror <roey.ghost@gmail.com>
2020-03-08 21:22:31 +0200

    Update oh-my-zsh plugins (fix #360)

--- src/execution_context.rs
+++ src/execution_context.rs
@@ -1,6 +1,7 @@
 #![allow(dead_code)]
 use crate::config::Config;
 use crate::executor::RunType;
+use crate::git::Git;
 use directories::BaseDirs;
 #[cfg(unix)]
 use std::path::PathBuf;
@@ -9,6 +10,7 @@ pub struct ExecutionContext<'a> {
     run_type: RunType,
     #[cfg(unix)]
     sudo: &'a Option<PathBuf>,
+    git: &'a Git,
     config: &'a Config,
     base_dirs: &'a BaseDirs,
 }
@@ -18,28 +20,35 @@ impl<'a> ExecutionContext<'a> {
     pub fn new(
         run_type: RunType,
         sudo: &'a Option<PathBuf>,
+        git: &'a Git,
         config: &'a Config,
         base_dirs: &'a BaseDirs,
     ) -> ExecutionContext<'a> {
         ExecutionContext {
             run_type,
             sudo,
+            git,
             config,
             base_dirs,
         }
     }
 
     #[cfg(not(unix))]
-    pub fn new(run_type: RunType, config: &'a Config, base_dirs: &'a BaseDirs) -> ExecutionContext<'a> {
+    pub fn new(run_type: RunType, git: &'a Git, config: &'a Config, base_dirs: &'a BaseDirs) -> ExecutionContext<'a> {
         ExecutionContext {
             run_type,
             config,
+            git,
             base_dirs,
         }
     }
 
     pub fn run_type(&self) -> RunType {
         self.run_type
+    }
+
+    pub fn git(&self) -> &Git {
+        &self.git
     }
 
     #[cfg(unix)]
