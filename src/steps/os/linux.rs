49cff62206745a113dbd51abace0cbd0f0e15dd7
che <che27012011@googlemail.com>
2020-06-15 13:25:11 +0200

    os-check: check id_like for fedora

--- src/steps/os/linux.rs
+++ src/steps/os/linux.rs
@@ -52,6 +52,8 @@ impl Distribution {
                 return Ok(Distribution::Suse);
             } else if id_like.contains(&"arch") || id_like.contains(&"archlinux") {
                 return Ok(Distribution::Arch);
+            } else if id_like.contains(&"fedora") {
+                return Ok(Distribution::Fedora);
             }
         }
 
