57e6dd59038fb6b374dca83419e587e4beacd500
Ed Heltzel <ed@rainyday.media>
2021-01-29 09:11:46 -0500

    resolves a failed fisher update - fisher no longer supports `fisher update` the new command is to just run `fisher`

--- src/steps/os/unix.rs
+++ src/steps/os/unix.rs
@@ -72,7 +72,7 @@ pub fn run_fisher(base_dirs: &BaseDirs, run_type: RunType) -> Result<()> {
 
     print_separator("Fisher");
 
-    run_type.execute(&fish).args(&["-c", "fisher update"]).check_run()
+    run_type.execute(&fish).args(&["-c", "fisher"]).check_run()
 }
 
 pub fn run_oh_my_fish(ctx: &ExecutionContext) -> Result<()> {
