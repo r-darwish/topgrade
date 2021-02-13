8af7910df01fb6161b5f662be43ac152b6131dd5
Roey Darwish Dror <roey.ghost@gmail.com>
2021-02-13 07:05:58 +0200

    Fix WSL

--- src/steps/os/windows.rs
+++ src/steps/os/windows.rs
@@ -53,7 +53,7 @@ pub fn run_scoop(cleanup: bool, run_type: RunType) -> Result<()> {
 pub fn run_wsl_topgrade(ctx: &ExecutionContext) -> Result<()> {
     let wsl = require("wsl")?;
     let topgrade = Command::new(&wsl)
-        .args(&["bash", "-l", "which", "topgrade"])
+        .args(&["bash", "-lc", "which topgrade"])
         .check_output()
         .map_err(|_| SkipStep(String::from("Could not find Topgrade installed in WSL")))?;
 
