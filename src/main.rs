6d130f07cb4d7288e6cbf16fc67b475fdd544599
Roey Darwish Dror <roey.ghost@gmail.com>
2021-02-17 19:41:52 +0200

    Fix compilation in FreeBSD (fix #641)

--- src/main.rs
+++ src/main.rs
@@ -277,9 +277,9 @@ fn run() -> Result<()> {
         target_os = "netbsd",
         target_os = "dragonfly"
     )))]
+    runner.execute(Step::Atom, "apm", || generic::run_apm(run_type))?;
     runner.execute(Step::VSCode, "vscode", || generic::run_vscode(run_type))?;
     runner.execute(Step::Fossil, "fossil", || generic::run_fossil(run_type))?;
-    runner.execute(Step::Atom, "apm", || generic::run_apm(run_type))?;
     runner.execute(Step::Rustup, "rustup", || generic::run_rustup(&base_dirs, run_type))?;
     runner.execute(Step::Dotnet, ".NET", || generic::run_dotnet_upgrade(&ctx))?;
     runner.execute(Step::Choosenim, "choosenim", || generic::run_choosenim(&ctx))?;
