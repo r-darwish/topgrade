a6d8cff227a866fd0c8cd6b823b52435ef536ae1
Alexander Willner <alex@willner.ws>
2021-03-01 17:41:38 +0100

    pip3 support

--- src/steps/generic.rs
+++ src/steps/generic.rs
@@ -220,6 +220,13 @@ pub fn run_pipx_update(run_type: RunType) -> Result<()> {
     run_type.execute(&pipx).arg("upgrade-all").check_run()
 }
 
+pub fn run_pip3_update(run_type: RunType) -> Result<()> {
+    let pip3 = utils::require("pip3")?;
+    print_separator("pip3");
+
+    run_type.execute(&pip3).args(&["install", "--upgrade", "pip"]).check_run()
+}
+
 pub fn run_stack_update(run_type: RunType) -> Result<()> {
     let stack = utils::require("stack")?;
     print_separator("stack");
