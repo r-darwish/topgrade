6056ff2df5c28f04ec44fc472c967e96e55b5411
Rory OConnell <19547+RoryO@users.noreply.github.com>
2021-04-07 12:01:31 -0700

    create kdeneon distribution

--- src/steps/os/linux.rs
+++ src/steps/os/linux.rs
@@ -36,6 +36,7 @@ pub enum Distribution {
     Solus,
     Exherbo,
     NixOS,
+    KDENeon,
 }
 
 impl Distribution {
@@ -55,6 +56,7 @@ impl Distribution {
             Some("gentoo") => Distribution::Gentoo,
             Some("exherbo") => Distribution::Exherbo,
             Some("nixos") => Distribution::NixOS,
+            Some("neon") => Distribution::KDENeon,
             _ => {
                 if let Some(id_like) = id_like {
                     if id_like.contains(&"debian") || id_like.contains(&"ubuntu") {
@@ -101,6 +103,7 @@ impl Distribution {
             Distribution::Solus => upgrade_solus(&sudo, run_type),
             Distribution::Exherbo => upgrade_exherbo(&sudo, cleanup, run_type),
             Distribution::NixOS => upgrade_nixos(&sudo, cleanup, run_type),
+            Distribution::KDENeon => upgrade_neon(ctx),
         }
     }
 
@@ -353,43 +356,28 @@ fn upgrade_gentoo(ctx: &ExecutionContext) -> Result<()> {
 
 fn upgrade_debian(ctx: &ExecutionContext) -> Result<()> {
     if let Some(sudo) = &ctx.sudo() {
-        // KDE neon is ubuntu based but uses it's own manager, pkcon
-        // running apt update with KDE neon is an error
-        // in theory rpm based distributions use pkcon as well, though that
-        // seems rare
-        // if that ever comes up this should be a function and rework the
-        // upgrade function with a new Distribution::PackageKit or some such
-        if let Some(pkcon) = which("pkcon") {
-            let mut exe = ctx.run_type().execute(&sudo);
-            let mut cmd = exe.arg(&pkcon).arg("refresh").arg("update");
-            if ctx.config().cleanup() {
-                cmd = cmd.arg("--autoremove");
-            }
-            cmd.check_run()?;
-        } else {
-            let apt = which("apt-fast").unwrap_or_else(|| PathBuf::from("/usr/bin/apt-get"));
-            ctx.run_type().execute(&sudo).arg(&apt).arg("update").check_run()?;
+        let apt = which("apt-fast").unwrap_or_else(|| PathBuf::from("/usr/bin/apt-get"));
+        ctx.run_type().execute(&sudo).arg(&apt).arg("update").check_run()?;
+
+        let mut command = ctx.run_type().execute(&sudo);
+        command.arg(&apt).arg("dist-upgrade");
+        if ctx.config().yes() {
+            command.arg("-y");
+        }
+        if let Some(args) = ctx.config().apt_arguments() {
+            command.args(args.split_whitespace());
+        }
+        command.check_run()?;
+
+        if ctx.config().cleanup() {
+            ctx.run_type().execute(&sudo).arg(&apt).arg("clean").check_run()?;
 
             let mut command = ctx.run_type().execute(&sudo);
-            command.arg(&apt).arg("dist-upgrade");
+            command.arg(&apt).arg("autoremove");
             if ctx.config().yes() {
                 command.arg("-y");
             }
-            if let Some(args) = ctx.config().apt_arguments() {
-                command.args(args.split_whitespace());
-            }
             command.check_run()?;
-
-            if ctx.config().cleanup() {
-                ctx.run_type().execute(&sudo).arg(&apt).arg("clean").check_run()?;
-
-                let mut command = ctx.run_type().execute(&sudo);
-                command.arg(&apt).arg("autoremove");
-                if ctx.config().yes() {
-                    command.arg("-y");
-                }
-                command.check_run()?;
-            }
         }
     } else {
         print_warning("No sudo detected. Skipping system upgrade");
@@ -471,6 +459,31 @@ fn upgrade_nixos(sudo: &Option<PathBuf>, cleanup: bool, run_type: RunType) -> Re
         }
     } else {
         print_warning("No sudo detected. Skipping system upgrade");
+    }
+
+    Ok(())
+}
+
+fn upgrade_neon(ctx: &ExecutionContext) -> Result<()> {
+    // KDE neon is ubuntu based but uses it's own manager, pkcon
+    // running apt update with KDE neon is an error
+    // in theory rpm based distributions use pkcon as well, though that
+    // seems rare
+    // if that comes up we need to create a Distribution::PackageKit or some such
+    if let Some(sudo) = &ctx.sudo() {
+        let pkcon = which("pkcon").unwrap();
+        // pkcon ignores update with update and refresh provided together
+        ctx.run_type().execute(&sudo).arg(&pkcon).arg("refresh").check_run()?;
+        let mut exe = ctx.run_type().execute(&sudo);
+        let cmd = exe.arg(&pkcon).arg("update");
+        if ctx.config().yes() {
+            cmd.arg("-y");
+        }
+        if ctx.config().cleanup() {
+            cmd.arg("--autoremove");
+        }
+        // from pkcon man, exit code 5 is 'Nothing useful was done.'
+        cmd.check_run_with_codes(&[5])?;
     }
 
     Ok(())
