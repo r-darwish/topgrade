258515620f812e60ea4b24cc2838644744b9a611
Roey Darwish Dror <roey.ghost@gmail.com>
2022-05-26 23:52:32 +0300

    Stop using packages.el (#942)
    Fixes #757

--- src/steps/emacs.el
+++ src/steps/emacs.el
@@ -1,4 +1,4 @@
-(if (fboundp 'paradox-upgrade-packages)
+(when (fboundp 'paradox-upgrade-packages)
     (progn
       (unless (boundp 'paradox-github-token)
         (setq paradox-github-token t))
@@ -6,11 +6,4 @@
       (princ
        (if (get-buffer "*Paradox Report*")
            (with-current-buffer "*Paradox Report*" (buffer-string))
-         "\nNothing to upgrade\n")))
-  (progn
-    (let ((package-menu-async nil))
-      (package-list-packages))
-    (package-menu-mark-upgrades)
-    (condition-case nil
-        (package-menu-execute 'noquery)
-      (user-error nil))))
+         "\nNothing to upgrade\n"))))
